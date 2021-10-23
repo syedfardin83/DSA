d27b8244aaf22568a51d8d5f10a7625bff758bf5
syedfardin83 <89456190+syedfardin83@users.noreply.github.com>
2021-10-23 15:36:22 +0530

    Create merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,67 @@
+#Arrays
+A = [7,9,12,14]
+B = [1,4,6,5]
+C = [0] * (len(A)+len(B))
+
+#Pointers
+i=0
+j=0
+k=0
+
+#sizes
+m = (len(A) - 1)
+n = (len(B) - 1) 
+o = (len(C) - 1)
+
+print(C)
+#print(C)
+
+
+while(i<=m and j<=n):
+    if(A[i] < B[j]):
+       C[k] = A[i]
+       i=i+1
+       k=k+1
+       print(C)
+       #print(f"i = {i}")
+       
+    if(A[i] > B[j]):
+        C[k] = B[j]
+        j=j+1
+        k=k+1
+        print(C)
+        #print(f"i = {i}")
+        
+    if(i>m):
+        break
+    
+    if(j>n):
+        break
+        
+while(i>m):
+    C[k] = B[j]
+    j=j+1
+    k=k+1
+    ##print(f"i = {i}")
+    
+    if(j>n):
+        break
+    
+while(j>n):
+    #print(i)
+    C[k] = A[i]
+    i=i+1
+    k=k+1 
+    ##print(f"i = {i}")
+    print(C)
+    
+    if(i>m):
+        break
+    
+print(C)   
+        
+    
+       
+       
+           
+    
